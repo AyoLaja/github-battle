dcb9c313d5fa85a1955525bd729112eb7cbcfd8e
Ayomide Onalaja <tuf22861@temple.edu>
2019-01-02 02:12:27 -0500

    done with component lifecycle

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,39 +1,109 @@
 var React = require('react')
+var PropTypes = require('prop-types')
+var api = require('../utils/api')
 
-class Popular extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedLanguage: 'All'
-        }
+function SelectLanguage(props) {
+    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 
-        this.updateLanguage = this.updateLanguage.bind(this)
-    }
-
-    updateLanguage(lang) {
-        this.setState(function() {
-            return {
-                selectedLanguage: lang
-            }
-        })
-    }
-
-    render() {
-        var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-
-        return (
-            <ul className="languages">
+    return (
+        <ul className="languages">
                 {languages.map(function(lang) {
                     return (
                         <li 
-                            style={lang === this.state.selectedLanguage ? { color: '#b00931'} : null}
-                            onClick={this.updateLanguage.bind(null, lang)}
+                            style={lang === props.selectedLanguage ? { color: '#b00931'} : null}
+                            onClick={props.onSelect.bind(null, lang)}
                             key={lang}>
                             {lang}
                         </li>
                     )
                 }, this)}
             </ul>
+    )
+}
+
+function RepoGrid(props) {
+    console.log(props.repos)
+
+    return (
+        <ul className="popular-list">
+            {props.repos.map(function(repo, index) {
+                return (
+                    <li key={repo.name} className="popular-item">
+                        <div className="popular-rank"># {index+1}</div>
+                        <ul className="space-list-item">
+                            <li>
+                                <img
+                                    className="avatar"
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li>
+                                <a href={repo.html_url}>{repo.name}</a>
+                            </li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+}
+
+class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All',
+            repos: null
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    componentDidMount() {
+        this.updateLanguage('All')
+    }
+
+    updateLanguage(lang) {
+        this.setState(function() {
+            return {
+                selectedLanguage: lang,
+                repos: null
+            }
+        })
+
+        api.fetchPopularRepos(lang)
+            .then((repos) => {
+                // console.log(repos)
+                this.setState(function() {
+                    return {
+                        repos: repos
+                    }
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <SelectLanguage 
+                    selectedLanguage={this.state.selectedLanguage} 
+                    onSelect={this.updateLanguage}
+                />
+                {!this.state.repos ? <p>Loading</p> : <RepoGrid repos={this.state.repos}/>}
+                {/* {JSON.stringify(this.state.repos, null, 2)} */}
+            </div>
         )
     }
 }
