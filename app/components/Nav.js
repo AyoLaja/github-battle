7f0dda50903e27939ebe24cb100ff7085f3dd36c
Ayomide Onalaja <tuf22861@temple.edu>
2019-01-05 17:41:22 -0500

    added nav logo

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -4,23 +4,30 @@ var NavLink = require('react-router-dom').NavLink
 
 function Nav() {
     return (
-        <ul className="nav">
-            <li>
-                <NavLink exact activeClassName="active" to="/">
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/battle">
-                    Battle
-                </NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/popular">
-                    Popular
-                </NavLink>
-            </li>
-        </ul>
+        <div className="nav-container">
+            <div className="nav-logo-container">
+                <h1 className="nav-logo">
+                    gb
+                </h1>
+            </div>
+            <ul className="nav">
+                <li>
+                    <NavLink exact activeClassName="active" to="/">
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/battle">
+                        Battle
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/popular">
+                        Popular
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
     )
 }
 
